c1a2276e93fe77583d35c7f6c4fd8767b00124a4
AhmedAbdrabou22 <ahmedabdrabou723@gmail.com>
2023-07-30 14:50:02 +0300

    Finish

--- src/components/utility/CategoryHeader.js
+++ src/components/utility/CategoryHeader.js
@@ -23,77 +23,13 @@ const CategoryHeader = () => {
     return (
         <Container fluid={true} style={{ background: "white" }}>
             <Navbar expand="lg" style={{ background: "white", alignItems: "center" }}>
-
-
-                {/* <Nav.Link className='mt-4'>
-                    <UnopDropdown
-                        onAppear={handler}
-                        onDisappearStart={handler}
-                        trigger={
-                            <p className="mx-1" style={{ border: "1px solid #0000001A", padding: "10px", marginTop: "-10px", textAlign: "right" }}>
-                                <img
-                                    width="20px"
-                                    height="20px"
-                                    className="ms-1"
-                                    src={cash}
-                                    alt=""
-                                />
-                                جميع المفات
-                            </p>
-                        }
-                        delay={0}
-                        align="right"
-                        hover>
-                        <div className="card-filter d-flex justify-content-between align-items-center">
-                            <div className="groubFilter">
-                                {
-                                    DataCate.data ? (
-                                        DataCate.data.slice(1, 7).map((ele) => {
-                                            return (
-                                                <div className="card-filter-item">
-                                                    <a className="cat-text-header" href={`/categorydetails/${ele.id}`}>{ele.title_ar}</a>
-                                                </div>
-                                            )
-                                        })
-                                    ) : null
-                                }
-                            </div>
-                            <div className="groubFilter">
-                                {
-                                    DataCate.data ? (
-                                        DataCate.data.slice(6, 12).map((ele) => {
-                                            return (
-                                                <div className="card-filter-item">
-                                                    <a className="cat-text-header" href={`/categorydetails/${ele.id}`}>{ele.title_ar}</a>
-                                                </div>
-                                            )
-                                        })
-                                    ) : null
-                                }
-                            </div>
-                            <div className="groubFilter">
-                                {
-                                    DataCate.data ? (
-                                        DataCate.data.slice(11, 17).map((ele) => {
-                                            return (
-                                                <div className="card-filter-item">
-                                                    <a className="cat-text-header" href={`/categorydetails/${ele.id}`}>{ele.title_ar}</a>
-                                                </div>
-                                            )
-                                        })
-                                    ) : null
-                                }
-                            </div>
-                        </div>
-                    </UnopDropdown>
-                </Nav.Link> */}
                 <Dropdown>
                     <Dropdown.Toggle variant="light" id="dropdown-basic">
                         جميع المفات
                     </Dropdown.Toggle>
 
                     <Dropdown.Menu align="right">
-                        <div className="card-filter d-flex justify-content-between align-items-center">
+                        <div className="card-filter d-flex justify-content-center align-items-center">
                             <div className="groubFilter">
                                 {
                                     DataCate.data ? (
@@ -122,7 +58,7 @@ const CategoryHeader = () => {
                                 }
                             </div>
 
-                            <div className="groubFilter">
+                            {/* <div className="groubFilter">
                                     {
                                         DataCate.data ? (
                                             DataCate.data.slice(10, 17).map((ele) => {
@@ -134,7 +70,7 @@ const CategoryHeader = () => {
                                             })
                                         ) : null
                                     }
-                                </div>
+                                </div> */}
                         </div>
                     </Dropdown.Menu>
                 </Dropdown>
