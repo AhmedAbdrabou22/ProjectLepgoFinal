3e28ea133c50d734f8fa2dcf86bc6420258a009e
AhmedAbdrabou22 <ahmedabdrabou723@gmail.com>
2023-08-01 12:22:48 +0300

    data4

--- src/components/Cards/RentedCard.js
+++ src/components/Cards/RentedCard.js
@@ -5,7 +5,7 @@ import { Card, Col } from 'react-bootstrap'
 const RentedCard = () => {
     return (
         <Col className="d-flex justify-content-around ">
-            <Card style={{ background: "#F9F9FA", borderRadius: "20px", transition: "0.3s" , marginTop:"-50px" , border:"1px solid red" }}>
+            <Card style={{ background: "#F9F9FA", borderRadius: "20px", transition: "0.3s" , marginTop:"-50px"}}>
                 <Card.Img variant="top" src={bicycle} style={{width:"100%"}}/>
                 <Card.Body>
                     <Card.Title>خصم%25</Card.Title>
